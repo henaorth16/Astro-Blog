17503f8416292add80e21d4135c928cc2f696284
henaorth16 <emyayehenok@gmail.com>
2024-01-17 04:21:08 +0300

    some features added

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export const SITE_TITLE = ' TeachPeople Blog';
+export const SITE_DESCROPTION = 'Articles, stories and tutorials from Tech People';
+export const HOME_PAGE_ARTICLE_LIMIT = 7;
+export const ARTICLES_PER_PAGE = 6;
+
