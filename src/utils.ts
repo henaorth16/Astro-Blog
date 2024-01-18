c087c7ee6b7ffa8772f29cf5d99bf9d803371f52
henaorth16 <emyayehenok@gmail.com>
2024-01-18 23:59:49 +0300

    minimizing hard code by iterating the md file data

---
+++ src/utils.ts
@@ -0,0 +1,9 @@
+function formatDate(date: Date): string {
+    const options: Intl.DateTimeFormatOptions = {year: 'numeric', month: "short", day: 'numeric'}
+  
+    return new Date(date).toLocaleDateString(undefined, options)
+  }
+
+  export {
+    formatDate
+  }
