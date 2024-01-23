37cb98ee43afaf1da71bf3c92ba16ebc0c7ea7b4
henaorth16 <emyayehenok@gmail.com>
2024-01-23 23:59:26 +0300

    new feature added

--- src/utils.ts
+++ src/utils.ts
@@ -4,6 +4,15 @@ function formatDate(date: Date): string {
     return new Date(date).toLocaleDateString(undefined, options)
   }
 
+
+function capitalize(str: string): string {
+  if(typeof str !== "string" || str.length === 0){
+    return str
+  }
+
+  return str.charAt(0).toUpperCase() + str.slice(1)
+}
+
   export {
     formatDate
   }
