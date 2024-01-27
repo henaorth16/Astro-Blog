9365c63bf9e3dc8da57b19185d3fbc85a1113c87
henaorth16 <emyayehenok@gmail.com>
2024-01-27 10:10:02 +0300

    final version

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const SITE_TITLE = ' TeachPeople Blog';
 export const SITE_DESCROPTION = 'Articles, stories and tutorials from Tech People';
 export const HOME_PAGE_ARTICLE_LIMIT = 6;
-export const ARTICLES_PER_PAGE = 2;
+export const ARTICLES_PER_PAGE = 3;
 
